285549a586fd79767e230544f8f42b6155b29557
modern@cleverco.ru <modern@cleverco.ru>
2024-06-12 13:49:18 +0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -25,44 +25,4 @@ System.Console.WriteLine(NumbersRec1(m, n));
 
    
 
-//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два
-// неотрицательных числа m и n. ПРИМЕР: m = 2, n = 3 -> A(m,n) = 29
-
-//static uint Akk(uint m, uint n)
-//{
-//    if (m == 0)
-//    {
-//        return n + 1;
-//    }
-//    else if (n == 0)
-//    {
-//        return Akk(m - 1, 1);
-//    }
-//    else
-//   {
-//        return Akk(m - 1, Akk(m, n - 1));
-//    }
-//}
- 
-//Console.WriteLine(Akk(3, 2)); 
-//Console.ReadKey();
-
-
-//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы
-//пример:[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
-
-//void OutpetElementsInReverseOrder(int[] array, int i = 0)
-//{
-//    if (i < array.Length)
-//    {
-//        OutpetElementsInReverseOrder(array, i + 1);
-//        Console.Write(array[i] + " ");
-//    }
-//}
-//int[] array = { 1, 2, 5, 0, 10, 34 };
-//OutpetElementsInReverseOrder(array);
-
-
-
-
 
