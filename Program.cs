6b594e786186a1ce132757eba7b17f8ab2cfc129
modern@cleverco.ru <modern@cleverco.ru>
2024-06-12 14:01:28 +0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 //Использовать рекурсию, не использовать циклы. 
-//Пример  M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+//Пример:  M = 1; N = 5 -> "1, 2, 3, 4, 5" 
 //M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
 int ReadInt(string argument)  
@@ -23,6 +23,41 @@ int m = ReadInt("Введите M: ");
 int n = ReadInt("Введите N: ");
 Console.WriteLine(NumbersRec1(m, n));
 
-   
+ //Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. ПРИМЕР: m = 2, n = 3 -> A(m,n) = 29
+
+//static uint Akk(uint m, uint n)
+//{
+//    if (m == 0)
+//    {
+//        return n + 1;
+//    }
+//    else if (n == 0)
+//    {
+//        return Akk(m - 1, 1);
+//    }
+//    else
+//   {
+//        return Akk(m - 1, Akk(m, n - 1));
+//    }
+//}
+ 
+//Console.WriteLine(Akk(3, 2)); 
+//Console.ReadKey();
+
+//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы
+//Пример:[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+//void OutpetElementsInReverseOrder(int[] array, int i = 0)
+//{
+//    if (i < array.Length)
+//    {
+//        OutpetElementsInReverseOrder(array, i + 1);
+//        Console.Write(array[i] + " ");
+//    }
+//}
+//int[] array = { 1, 2, 5, 0, 10, 34 };
+//OutpetElementsInReverseOrder(array);
+  
 
 
